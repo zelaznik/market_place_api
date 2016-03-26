b9e6dd9bfde863e48ad1b7accca85a57c61cf09e
Steve Zelaznik <zelaznik@yahoo.com>
2016-03-26 16:43:06 -0400

    Adds show action to the users controller.

--- config/application.rb
+++ config/application.rb
@@ -18,17 +18,17 @@ Bundler.require(*Rails.groups)
 module MarketPlaceApi
   class Application < Rails::Application
     # don't generate RSpec tests for views and helpers
-  config.generators do |g|
-    g.test_framework :rspec, fixture: true
-    g.fixture_replacement :factory_girl, dir: 'spec/factories'
-    g.view_specs false
-    g.helper_specs false
-    g.stylesheets = false
-    g.javascripts = false
-    g.helper = false
-  end
-  
-  config.autoload_paths += %W(#{config.root}/lib)
+    config.generators do |g|
+      g.test_framework :rspec, fixture: true
+      g.fixture_replacement :factory_girl, dir: 'spec/factories'
+      g.view_specs false
+      g.helper_specs false
+      g.stylesheets = false
+      g.javascripts = false
+      g.helper = false
+    end
+
+    config.autoload_paths += %W(#{config.root}/lib)
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -43,5 +43,12 @@ module MarketPlaceApi
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+  end
+end
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
   end
 end
