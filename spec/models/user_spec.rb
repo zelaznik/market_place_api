c3749d037d42cdc517227338485881aaaee7f60c
Steve Zelaznik <zelaznik@yahoo.com>
2016-03-26 14:04:42 -0400

    Adds shoulda matchers for spec refactors

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,4 +9,9 @@ RSpec.describe User, type: :model do
   it { should respond_to(:password_confirmation) }
 
   it { should be_valid }
+
+  it { should validate_presence_of(:email) }
+  it { should validate_uniqueness_of(:email) }
+  it { should allow_value('examples@domain.com').for(:email) }
+
 end
