d56de386271ce878c5ab05d31cfe302550890c6a
Steve Zelaznik <zelaznik@yahoo.com>
2016-03-27 15:43:22 -0400

    Refactors test headers for each request

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -1,8 +1,6 @@
 require 'spec_helper'
 
 RSpec.describe API::V1::UsersController, type: :controller do
-  before(:each) { request.headers['Accept'] = 'application/vnd.marketplace.v1' }
-
   describe 'GET #show' do
     before(:each) do
       @user = FactoryGirl.create :user
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,10 +15,11 @@ RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
 
-  config.include Request::JsonHelpers, :type => :controller
+  config.include Request::JsonHelpers, type: :controller
+  config.include Request::HeadersHelpers, type: :controller
 
-  config.include(Shoulda::Matchers::ActiveModel, type: :model)
-  config.include(Shoulda::Matchers::ActiveRecord, type: :model)
+  config.include Shoulda::Matchers::ActiveModel, type: :model
+  config.include Shoulda::Matchers::ActiveRecord, type: :model
 
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
@@ -27,4 +28,10 @@ RSpec.configure do |config|
   config.mock_with :rspec do |mocks|
     mocks.verify_partial_doubles = true
   end
+
+  config.before :each, type: :controller do
+    include_default_accept_headers
+  end
+
+
 end
